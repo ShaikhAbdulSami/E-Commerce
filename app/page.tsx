9f5e3bde77d33556391c3cc834b52dba54b098cb
Shaikh Abdul Sami <65606480+ShaikhAbdulSami@users.noreply.github.com>
2023-07-03 16:23:02 +0500

    Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Header from '@/components/Header'
-import Image from 'next/image'
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return (
-    <div>
-      <Header/>
-    </div>
-  )
+	return (
+		<div>
+			<Hero />
+		</div>
+	);
 }
