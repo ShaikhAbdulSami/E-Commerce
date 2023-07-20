630aed12ef1870e32a38393ac0b6aa5b830c73c3
Shaikh Abdul Sami <108601988+shaikh-piaic@users.noreply.github.com>
2023-07-20 19:07:09 +0500

    Cart API

---
+++ app/api/cart/route.ts
@@ -0,0 +1,37 @@
+import { cartTable, db } from "@/lib/drizzle";
+import { cookies } from "next/dist/client/components/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { v4 as uuid } from "uuid";
+export const GET = async (request: Request) => {
+	try {
+		const res = await db.select().from(cartTable);
+		return NextResponse.json({ res });
+	} catch (error) {
+		return NextResponse.json({ error });
+	}
+};
+
+export const POST = async (request: Request) => {
+	const req = await request.json();
+
+	const uid = uuid();
+	const setCookies = cookies();
+	const user_id = cookies().get("user_id")?.value;
+	if (!user_id) {
+		setCookies.set("user_id", uid);
+	}
+
+	try {
+		const res = await db
+			.insert(cartTable)
+			.values({
+				product_id: req.product_id,
+				quantity: 1,
+				user_id: cookies().get("user_id")?.value as string,
+			})
+			.returning();
+		return NextResponse.json({ res });
+	} catch (error) {
+		return NextResponse.json({ error });
+	}
+};
