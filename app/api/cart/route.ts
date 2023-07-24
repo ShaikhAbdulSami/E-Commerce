f857bef4c622b2aa3bdfa724e39efe006e7eb9ab
Shaikh Abdul Sami <108601988+shaikh-piaic@users.noreply.github.com>
2023-07-24 18:58:43 +0500

    Update route.ts

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -1,11 +1,10 @@
-import { cartTable, db } from "@/lib/drizzle";
-import { eq } from "drizzle-orm";
+import { Cart, cartTable, db } from "@/lib/drizzle";
+import { and, eq } from "drizzle-orm";
 import { cookies } from "next/dist/client/components/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { v4 as uuid } from "uuid";
-export const GET = async (request: Request) => {
+export const GET = async () => {
 	const user_id = cookies().get("user_id")?.value as string;
-
 	try {
 		const res = await db
 			.select()
@@ -16,27 +15,100 @@ export const GET = async (request: Request) => {
 		return NextResponse.json({ error });
 	}
 };
-
-export const POST = async (request: Request) => {
+export const POST = async (request: NextRequest) => {
 	const req = await request.json();
 
 	const uid = uuid();
 	const setCookies = cookies();
-	const user_id = cookies().get("user_id")?.value;
+	const user_id = cookies().get("user_id")?.value as string;
 	if (!user_id) {
 		setCookies.set("user_id", uid);
 	}
 
 	try {
-		const res = await db
-			.insert(cartTable)
-			.values({
-				product_id: req.product_id,
-				quantity: 1,
-				user_id: cookies().get("user_id")?.value as string,
-			})
-			.returning();
-		return NextResponse.json({ res });
+		const existingCartItem = await db
+			.select()
+			.from(cartTable)
+			.where(
+				and(
+					eq(cartTable.user_id, user_id),
+					eq(cartTable.product_id, req.product_id)
+				)
+			)
+			.execute();
+
+		if (existingCartItem.length > 0) {
+			// If the product already exists in the cart, increase the quantity
+			const q = existingCartItem.map((itm) => {
+				return itm.quantity;
+			});
+			const res = await db
+				.update(cartTable)
+				.set({
+					quantity: q[0] + req.quantity,
+				})
+				.where(
+					and(
+						eq(cartTable.user_id, user_id),
+						eq(cartTable.product_id, req.product_id)
+					)
+				)
+				.returning();
+
+			return NextResponse.json({ res });
+		} else {
+			const res = await db
+				.insert(cartTable)
+				.values({
+					product_id: req.product_id,
+					quantity: req.quantity,
+					price: req.price,
+					user_id: cookies().get("user_id")?.value as string,
+				})
+				.returning();
+			return NextResponse.json({ res });
+		}
+	} catch (error) {
+		return NextResponse.json({ error });
+	}
+};
+
+export const PUT = async (request: NextRequest) => {
+	const req = await request.json();
+	const user_id = cookies()?.get("user_id")?.value as string;
+
+	try {
+		const existingCartItem = await db
+			.select()
+			.from(cartTable)
+			.where(
+				and(
+					eq(cartTable.user_id, user_id),
+					eq(cartTable.product_id, req.product_id)
+				)
+			)
+			.execute();
+
+		if (existingCartItem.length > 0) {
+			// If the product already exists in the cart, increase the quantity
+			const q = existingCartItem.map((itm) => {
+				return itm.quantity;
+			});
+			const res = await db
+				.update(cartTable)
+				.set({
+					quantity: q[0] + req.quantity,
+				})
+				.where(
+					and(
+						eq(cartTable.user_id, user_id),
+						eq(cartTable.product_id, req.product_id)
+					)
+				)
+				.returning();
+
+			return NextResponse.json({ res });
+		}
 	} catch (error) {
 		return NextResponse.json({ error });
 	}
