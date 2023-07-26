1e5248b64f5cb25d2c00f859370b5aea275483b2
Shaikh Abdul Sami <108601988+shaikh-piaic@users.noreply.github.com>
2023-07-27 01:33:46 +0500

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,17 +4,35 @@ import { Button } from "./ui/button";
 import Image from "next/image";
 import logo from "/public/Logo.webp";
 import { Menu } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { RootState } from "@/redux/store";
+import { clearCart, getCart } from "@/redux/slice/cartSlice";
 
 const Navbar = () => {
+	const dispatch = useAppDispatch();
 	const [open, setOpen] = useState(false);
+	const getProduct = async () => {
+		const res = await fetch("/api/cart", { method: "GET" });
+		const result = await res.json();
+		// console.log(result.res);
+		dispatch(clearCart());
+		dispatch(getCart());
+	};
 	const menuLinks = [
 		{ name: "Female", link: "/female" },
 		{ name: "Male", link: "/male" },
 		{ name: "Kids", link: "/kids" },
 		{ name: "All Products", link: "/products" },
 	];
+	const qty = useAppSelector(
+		(state: RootState) => state.CartSlice.totalQuantity
+	);
+	useEffect(() => {
+		dispatch(clearCart());
+		getProduct();
+	}, []);
 	return (
 		<>
 			<nav className='justify-between items-center lg:px-24 lg:py-8 md:p-16 p-8 flex mx-auto container'>
@@ -32,12 +50,14 @@ const Navbar = () => {
 					<li className='list-none cursor-pointer'>Kids</li>
 					<li className='list-none cursor-pointer'>All Products</li> */}
 				</ul>
-				<Button className='lg:flex hidden w-12 h-22 bg-[#f1f1f1] rounded-full border-none relative transition delay-300 ease-in-out'>
-					<ShoppingCart color='black' />
-					<span className='absolute top-0 right-1 text-white bg-red-600 w-4 h-4 rounded-lg text-center font-semibold text-xs'>
-						0
-					</span>
-				</Button>
+				<Link href={"/cart"}>
+					<Button className='lg:flex hidden w-12 h-22 bg-[#f1f1f1] rounded-full border-none relative transition delay-300 ease-in-out'>
+						<ShoppingCart color='black' />
+						<span className='absolute top-0 right-1 text-white bg-red-600 w-4 h-4 rounded-lg text-center font-semibold text-xs'>
+							{qty}
+						</span>
+					</Button>
+				</Link>
 				{/* Mobile Navbar */}
 
 				<div className='flex lg:hidden'>
@@ -64,12 +84,14 @@ const Navbar = () => {
 								className=' top-[50px] right-[50px] absolute '
 							/>
 							<ul className={`flex gap-y-4 flex-col text-center items-center`}>
-								<Button className='flex w-12 h-22 bg-[#f1f1f1] rounded-full border-none relative transition delay-300 ease-in-out'>
-									<ShoppingCart color='black' />
-									<span className='absolute top-0 right-1 text-white bg-red-600 w-4 h-4 rounded-lg text-center font-semibold text-xs'>
-										0
-									</span>
-								</Button>
+								<Link href={"/cart"}>
+									<Button className='flex w-12 h-22 bg-[#f1f1f1] rounded-full border-none relative transition delay-300 ease-in-out'>
+										<ShoppingCart color='black' />
+										<span className='absolute top-0 right-1 text-white bg-red-600 w-4 h-4 rounded-lg text-center font-semibold text-xs'>
+											{qty}
+										</span>
+									</Button>
+								</Link>
 								{menuLinks?.map((link, i) => (
 									<Link href={link.link} key={i}>
 										<li
