ff771e6dc463a1d3b2993f2fa0170779162161fa
Shaikh Abdul Sami <108601988+shaikh-piaic@users.noreply.github.com>
2023-08-18 20:52:09 +0500

    Delete API Created

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -113,3 +113,38 @@ export const PUT = async (request: NextRequest) => {
 		return NextResponse.json({ error });
 	}
 };
+export const DELETE = async (request: NextRequest) => {
+	// const req = await request.json();
+	const user_id = cookies()?.get("user_id")?.value as string;
+	const id = request.nextUrl.searchParams.get("product_id") as string;
+	console.log(user_id);
+	try {
+		const existingCartItem = await db
+			.select()
+			.from(cartTable)
+			.where(and(eq(cartTable.user_id, user_id), eq(cartTable.product_id, id)))
+			.execute();
+
+		if (existingCartItem.length > 0) {
+			// If the product exists in the cart, delete the item
+			const res = await db
+				.delete(cartTable)
+				.where(
+					and(eq(cartTable.user_id, user_id), eq(cartTable.product_id, id))
+				)
+				.returning();
+
+			return NextResponse.json({
+				success: true,
+				message: "Item deleted successfully",
+			});
+		} else {
+			return NextResponse.json({
+				success: false,
+				message: "Item not found in cart",
+			});
+		}
+	} catch (error) {
+		return NextResponse.json({ success: false, error });
+	}
+};
